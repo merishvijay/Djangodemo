07f06b693f306d271ebbe2a8e53abe7cc91bfaad
merishvijay <merishmittu@gmail.com>
2024-02-06 09:35:48 +0530

    added

--- ecommerce/cart/admin.py
+++ ecommerce/cart/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 from cart.models import Cart,Account,Order
 
+from django.http import HttpResponse
+
 admin.site.register(Cart)
 admin.site.register(Account)
 admin.site.register(Order)
